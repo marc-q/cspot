f3c0fb97624dbb49dcb6452daf4b14869542e8d7
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2016-07-29 14:17:17 +0200

    Create cspot_filter.c
    This file contains all the filtering stuff from now on.

---
+++ src/cspot_filter.c
@@ -0,0 +1,34 @@
+/* Copyright 2016 Marc Volker Dickmann */
+/* Project: CSpot */
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include "cspot_log.h"
+#include "cspot_filter.h"
+
+bool cspot_filter_is_empty (char *str)
+{
+	unsigned int i;
+	
+	for (i = 0; i < strlen (str); i++)
+	{
+		if (isspace (str[i]) == 0)
+		{
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+bool cspot_filter_ending_char (char *str, char c)
+{
+	if (cspot_filter_is_empty (str) == false &&
+	    str[strlen (str)-2] == c)
+	{
+		return true;
+	}
+	
+	return false;
+}
