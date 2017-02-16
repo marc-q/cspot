eeed447fca93d805254ac1355a15b013a56504fb
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2017-02-16 16:33:45 +0100

    Update cspot_log.c
    Use my new code style.

--- src/cspot_log.c
+++ src/cspot_log.c
@@ -1,14 +1,17 @@
-/* Copyright 2016 - 2017 Marc Volker Dickmann */
-/* Project: CSpot */
+/* Copyright 2016 - 2017 Marc Volker Dickmann
+ * Project: CSpot
+ */
 #include <stdio.h>
 #include "cspot_log.h"
 
-void cspot_log_error (char *description)
+void
+cspot_log_error (const char *description)
 {
 	printf ("Error: %s\n", description);
 }
 
-void cspot_log_highlight (char *description, unsigned int line, unsigned int column)
+void
+cspot_log_highlight (const char *description, const size_t line, const size_t column)
 {
-	printf ("Highlight: %s at L: %i C: %i.\n", description, line, column);
+	printf ("Highlight: %s at L: %zu C: %zu.\n", description, line, column);
 }
