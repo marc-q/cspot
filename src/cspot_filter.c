2bbf5b786fcdf280453673fa6075bf6537afebf2
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2017-02-16 16:31:39 +0100

    Update cspot_filter.c
    Use my new code style.

--- src/cspot_filter.c
+++ src/cspot_filter.c
@@ -1,33 +1,34 @@
-/* Copyright 2016 - 2017 Marc Volker Dickmann */
-/* Project: CSpot */
+/* Copyright 2016 - 2017 Marc Volker Dickmann
+ * Project: CSpot
+ */
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
 #include "cspot_filter.h"
 
-bool cspot_filter_is_empty (char *str)
+bool
+cspot_filter_is_empty (const char *str)
 {
-	unsigned int i;
+	const size_t len = strlen (str);
 	
-	for (i = 0; i < strlen (str); i++)
+	for (size_t i = 0; i < len; i++)
 	{
 		if (isspace (str[i]) == 0)
 		{
 			return false;
 		}
 	}
-	
 	return true;
 }
 
-bool cspot_filter_ending_char (char *str, char c)
+bool
+cspot_filter_ending_char (const char *str, const char c)
 {
-	if (cspot_filter_is_empty (str) == false &&
-	    str[strlen (str)-2] == c)
+	if (!cspot_filter_is_empty (str) &&
+	    str[strlen (str) - 2] == c)
 	{
 		return true;
 	}
-	
 	return false;
 }
